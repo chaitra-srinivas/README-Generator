1634ee241eef42c5c65beb15e81d18f64ee87169
Chaitra <chaitra.srinivas@gmail.com>
2021-10-15 17:02:15 +1100

    Updated to get the license badges and links

--- README.md
+++ README.md
@@ -1,17 +1,28 @@
-# Absolutely Terrific!
-## It's cool
+# README Generator [![License: MIT](https://img.shields.io/badge/License-MIT-yellow.svg)](https://opensource.org/licenses/MIT)
+## Overview
+Using this application a developer can generate a professional README file by entering a few detaild
 
-## Table of Contents (Optional)
-If your README is long, add a table of contents to make it easy for users to find what they need.
+## Table of Contents
+
 - [Installation](#installation)
 - [Usage](#usage)
-- [Credits](#credits)
+- [Contributors](#contributors)
+- [Tests](#tests)
+- [Questions](#questions)
+- [E-mail](#email)
 - [License](#license)
+
 ## Installation
-gg
+Please install VS and node.js 
 ## Usage
-ffgg
-## Credits
-gg
+This application can be used by installing node.js
+## Contributing
+Chaitra
+## Tests
+Tests are perfomed
+## Questions
+GitHub Profile: [chaitrs-srinivas](https://github.com/chaitrs-srinivas)
+## E-mail
+cmurthydev@gmail.com
 ## License
 MIT
--- index.js
+++ index.js
@@ -17,64 +17,126 @@ const questions = () => {
       message: "Describe your project:",
     },
     {
-        type: "input",
-        name: "usage",
-        message: "Usage Information:",
-      },
-      {
-        type: "input",
-        name: "installation",
-        message: "Installation Instructions:",
-      },
-      {
-        type: "input",
-        name: "contributors",
-        message: "Contibutors to the project:",
-      },
-      {
-        type: "input",
-        name: "tests",
-        message: "Tests for the project:",
-      },
-      {
-        type: "checkbox",
-        name: "licence",
-        choices: ['MIT','GNU GPLv3','GNU AGPLv3','GNU LGPLv3','Apache License 2.0'],
-      },
+      type: "input",
+      name: "usage",
+      message: "Usage Information:",
+    },
+    {
+      type: "input",
+      name: "installation",
+      message: "Installation Instructions:",
+    },
+    {
+      type: "input",
+      name: "contributors",
+      message: "Contibutors to the project:",
+    },
+    {
+      type: "input",
+      name: "tests",
+      message: "Tests for the project:",
+    },
+    {
+      type: "input",
+      name: "github",
+      message: "What is your GitHub username?",
+    },
+    {
+      type: "input",
+      name: "email",
+      message: "What is your email address?",
+    },
+    {
+      type: "checkbox",
+      name: "license",
+      choices: [
+        "MIT",
+        "GNU GPLv3",
+        "GNU AGPLv3",
+        "GNU LGPLv3",
+        "Apache License 2.0",
+      ],
+    },
   ]);
 };
 
-/* 
-// TODO: Create a function to write README file
-function writeToFile(fileName, data) {} */
-
 // A function to write README file
-const writeToFile = ({title,description,usage,installation,contributors,tests,licence}) =>
-`# ${title}
-## ${description}
+const writeToFile = ({
+  title,
+  description,
+  usage,
+  installation,
+  contributors,
+  tests,
+  github,
+  email,
+  license,
+}) =>
+  `# ${title} ${generateLicenseBadge(license[0])}
+## Overview
+${description}
 
-## Table of Contents (Optional)
-If your README is long, add a table of contents to make it easy for users to find what they need.
+## Table of Contents
+
 - [Installation](#installation)
 - [Usage](#usage)
-- [Credits](#credits)
+- [Contributors](#contributors)
+- [Tests](#tests)
+- [Questions](#questions)
+- [E-mail](#email)
 - [License](#license)
+
 ## Installation
 ${installation}
 ## Usage
 ${usage}
-## Credits
+## Contributing
 ${contributors}
+## Tests
+${tests}
+## Questions
+GitHub Profile: [${github}](https://github.com/${github})
+## E-mail
+${email}
 ## License
-${licence}`
+${license}
+`;
 
 //A function to initialize app
 function init() {
-    questions()
-    .then((answers)=>fs.writeFileSync('README.md',writeToFile(answers)))
-    .then(()=>console.log('Successfully generated README.md'))
-    .catch((err)=>console.log(err));
+  questions()
+    .then((answers) => fs.writeFileSync("README.md", writeToFile(answers)))
+    .then(() => console.log("Successfully generated README.md"))
+    .catch((err) => console.log(err));
 }
 
 // Function call to initialize app
 init();
+
+// Function to generate license badges
+
+function generateLicenseBadge(licenseType) {
+  let licenseString = "";
+
+  if (licenseType === "MIT") {
+    licenseString = `[![License: MIT](https://img.shields.io/badge/License-MIT-yellow.svg)](https://opensource.org/licenses/MIT)`;
+    console.log(licenseString);
+  }
+  if (licenseType === "GNU GPLv3") {
+    licenseString = `[![License: GPL v3](https://img.shields.io/badge/License-GPLv3-blue.svg)](https://www.gnu.org/licenses/gpl-3.0)`;
+    console.log(licenseString);
+  }
+  if (licenseType === "GNU AGPLv3") {
+    licenseString = `[![License: AGPL v3](https://img.shields.io/badge/License-AGPL%20v3-blue.svg)](https://www.gnu.org/licenses/agpl-3.0)`;
+    console.log(licenseString);
+  }
+  if (licenseType === "GNU LGPLv3") {
+    licenseString = `[![License: LGPL v3](https://img.shields.io/badge/License-LGPL%20v3-blue.svg)](https://www.gnu.org/licenses/lgpl-3.0)`;
+    console.log(licenseString);
+  }
+  if (licenseType === "Apache License 2.0") {
+    licenseString = `[![License](https://img.shields.io/badge/License-Apache%202.0-blue.svg)](https://opensource.org/licenses/Apache-2.0)`;
+    console.log(licenseString);
+  }
+  return licenseString;
+}
